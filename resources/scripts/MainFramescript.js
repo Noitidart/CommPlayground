e272bd6450bf9947139c078e2d05ec3a51e7a235
Noitidart <noitidart@gmail.com>
2016-08-18 22:15:13 -0700

    rev5 *Started restrcuture for jscSystemHotkey looking good just needed to get rid of Comm and bring it back

--- resources/scripts/MainFramescript.js
+++
@@ -1,2 +0,0 @@
-Services.scriptloader.loadSubScript('chrome://comm/content/resources/scripts/Comm/Comm.js');
-Services.scriptloader.loadSubScript('chrome://comm/content/resources/scripts/MainFramescriptSubscript.js');
